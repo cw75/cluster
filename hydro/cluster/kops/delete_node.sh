6e8315e8d6dbe0ed04904db108c914e5dbb88849
cw75 <cgwu0530@gmail.com>
2020-04-14 10:09:40 +0000

    update delete script

--- hydro/cluster/kops/delete_node.sh
+++ hydro/cluster/kops/delete_node.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-kubectl drain $1 --ignore-daemonsets --delete-local-data
+kubectl drain $1 --ignore-daemonsets --delete-local-data > /dev/null 2>&1
 
-kubectl delete node $1
+kubectl delete node $1 > /dev/null 2>&1
