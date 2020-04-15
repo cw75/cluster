b66e5d9898c2217eda18defbbc12224ee5cba380
cw75 <cgwu0530@gmail.com>
2020-04-15 02:06:54 +0000

    add region

--- hydro/cluster/kops/delete_node.sh
+++ hydro/cluster/kops/delete_node.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-kubectl drain $1 > /dev/null 2>&1
+kubectl drain $1 --ignore-daemonsets --delete-local-data > /dev/null 2>&1
 
 kubectl delete node $1 > /dev/null 2>&1
 
 ID=$(aws ec2 --region us-east-1 describe-instances --filter Name=private-dns-name,Values=$1 --query 'Reservations[].Instances[].InstanceId' --output text)
-aws ec2 terminate-instances --instance-ids $ID > /dev/null 2>&1
+aws ec2 --region us-east-1 terminate-instances --instance-ids $ID > /dev/null 2>&1
