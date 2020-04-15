35cceff5ea7cdb25c5cd66818e88042ab8507c95
cw75 <cgwu0530@gmail.com>
2020-04-15 01:10:13 +0000

    enable policy

--- hydro/cluster/kops/delete_node.sh
+++ hydro/cluster/kops/delete_node.sh
@@ -5,4 +5,4 @@ kubectl drain $1 > /dev/null 2>&1
 kubectl delete node $1 > /dev/null 2>&1
 
 ID=$(aws ec2 describe-instances --filter Name=private-dns-name,Values=$1 --query 'Reservations[].Instances[].InstanceId' --output text)
-aws ec2 terminate-instances --instance-ids $ID
+aws ec2 terminate-instances --instance-ids $ID > /dev/null 2>&1
