a07fdafe21125f0952ef92a7bc4f686845106d77
cw75 <cgwu0530@gmail.com>
2020-04-15 00:45:03 +0000

    delete aws, no sleep

--- hydro/cluster/kops/delete_node.sh
+++ hydro/cluster/kops/delete_node.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-kubectl drain $1 --ignore-daemonsets --delete-local-data > /dev/null 2>&1
+kubectl drain $1 > /dev/null 2>&1
 
 kubectl delete node $1 > /dev/null 2>&1
+
+ID=$(aws ec2 describe-instances --filter Name=private-dns-name,Values=$1 --query 'Reservations[].Instances[].InstanceId' --output text)
+aws ec2 terminate-instances --instance-ids $ID
